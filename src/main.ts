daf3a17c910d80cf80720471524b75d44d4d26be
Lorenzo Mihut <lorenzo.mihut@sitline.ro>
2024-12-29 14:07:39 +0200

    Added a navbar and a footer

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+
+import * as hbs from 'hbs';
+import * as hbsUtils from 'hbs-utils';
+
 import { join } from 'path';
 import { AppModule } from './app.module';
 
@@ -8,6 +12,9 @@ async function bootstrap() {
 
 	app.useStaticAssets(join(__dirname, '..', 'public'));
 	app.setBaseViewsDir(join(__dirname, '..', 'views'));
+	hbs.registerPartials(join(__dirname, '..', 'views/layouts'));
+	hbsUtils(hbs).registerWatchedPartials(join(__dirname, '..', 'views/layouts'));
+
 	app.setViewEngine('hbs');
 
 	await app.listen(process.env.PORT ?? 3000);
